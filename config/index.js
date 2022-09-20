8844b0ca9cb4f24f033743cf29a42cdfb0ca46d4
NieStars <113980861+NieStars@users.noreply.github.com>
2022-09-20 17:52:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oQuGB6o78-hmAYGtJFOzn4hdC--M",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "rfOAwd8WFnqRO9LbS8cZAyBMdu5KS03_aYsKT53m9as",
+      useTemplateId: "FIhxFMvqsIDqcd21KTAvQSHw7mW0gjerdCqDZ1TDY6Y",
       // 所在省份
       province: "重庆",
       // 所在城市
